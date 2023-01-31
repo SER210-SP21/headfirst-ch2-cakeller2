ab3c2794463dc09037412ec2628396330929a5e4
cakeller2 <cakeller2@quinnipiac.edu>
2023-01-31 13:46:53 -0500

    testing for the 5th time

--- app/src/main/java/edu/quinnipiac/ser210/beeradvisor/MainActivity.kt
+++ app/src/main/java/edu/quinnipiac/ser210/beeradvisor/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("please work")
+        println("please work, pretty please")
     }
 } //testing this once again
